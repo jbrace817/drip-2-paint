e8d76ae388a8cb0a7c1c3b9a503c5b5b401c7c2c
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-01-15 00:19:56 -0500

    add tailwind indicator

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from '@/components/home/About';
 import Hero from '@/components/home/Hero';
+import { TailwindIndicator } from '@/components/tailwind-indicator';
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <About />
+      <TailwindIndicator />
     </div>
   );
 }
