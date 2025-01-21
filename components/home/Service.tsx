941a6a94e65b6f3544cd8109a77fd7a432ae4d37
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-01-21 00:39:32 -0500

    Add Fade plugin and Alignment to pics and description
    Also will remove scroll bar from tabs

--- components/home/Service.tsx
+++ components/home/Service.tsx
@@ -10,47 +10,54 @@ import {
   CarouselApi,
 } from '@/components/ui/carousel';
 import { Button } from '@/components/ui/button';
+import Fade from 'embla-carousel-fade';
+import Image from 'next/image';
 
 const features = [
   {
     id: 'feature-1',
     tabLabel: 'Exteriors',
-    title: 'Cras sagittis sollicitudin lobortis.',
+    title: 'Exteriors',
     description:
-      'Nam vitae molestie arcu. Quisque eu libero orci. Aliquam imperdiet magna nec massa consectetur, id interdum ante congue.',
-    image: 'https://shadcnblocks.com/images/block/placeholder-1.svg',
+      'A quality paint job protects your home’s exterior and boosts curb appeal. At Drip Painting, we ensure a durable, beautiful finish with expert techniques and the best products.',
+    image:
+      'https://res.cloudinary.com/dsjx8ner3/image/upload/v1737435684/FP_exterior_blue_hmhgz7.webp',
   },
   {
     id: 'feature-2',
     tabLabel: 'Interiors',
-    title: 'Nunc facilisis elit sed erat egestas fringilla.',
+    title: 'Interiors',
     description:
-      'Nam vitae molestie arcu. Quisque eu libero orci. Aliquam imperdiet magna nec massa consectetur.',
-    image: 'https://shadcnblocks.com/images/block/placeholder-2.svg',
+      'Refresh your home effortlessly with Drip Painting. We deliver flawless results while protecting your space and ensuring a smooth, stress-free experience.',
+    image:
+      'https://images.unsplash.com/photo-1598928506311-c55ded91a20c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 'feature-3',
     tabLabel: 'Custom Finishes',
-    title: 'Morbi finibus est diam.',
+    title: 'Custom Finishes',
     description:
-      'Nam vitae molestie arcu. Quisque eu libero orci. Aliquam imperdiet magna nec massa consectetur, id interdum ante congue.',
-    image: 'https://shadcnblocks.com/images/block/placeholder-3.svg',
+      'Custom finishes add character and elegance to your space. At Drip Painting, we create unique textures and details, crafting a personalized look that reflects your style.',
+    image:
+      'https://images.unsplash.com/photo-1598928428433-1077478561d6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 'feature-4',
     tabLabel: 'Bathrooms',
-    title: 'Morbi finibus est diam.',
+    title: 'Bathrooms',
     description:
-      'Nam vitae molestie arcu. Quisque eu libero orci. Aliquam imperdiet magna nec massa consectetur, id interdum ante congue.',
-    image: 'https://shadcnblocks.com/images/block/placeholder-3.svg',
+      "Transform your bathroom into a stunning retreat with Drip Painting’s expert remodeling services. A bathroom remodel not only enhances your home's functionality and comfort but also increases its value.",
+    image:
+      'https://images.unsplash.com/photo-1543502999-b65be91f22ea?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 'feature-5',
     tabLabel: 'Kitchens',
-    title: 'Morbi finibus est diam.',
+    title: 'Kitchens',
     description:
-      'Nam vitae molestie arcu. Quisque eu libero orci. Aliquam imperdiet magna nec massa consectetur, id interdum ante congue.',
-    image: 'https://shadcnblocks.com/images/block/placeholder-3.svg',
+      'Updating your kitchen’s design is essential, but making it fit your lifestyle is just as important. At Drip Painting, we craft kitchens that are both stunning and functional.',
+    image:
+      'https://images.unsplash.com/photo-1539922812788-4ea8cb53ba45?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
 ];
 
@@ -145,7 +152,7 @@ const Service = () => {
         <div className="mx-auto px-4 container">
           <Tabs value={selection} onValueChange={setSelection}>
             <div className="relative">
-              <div className="overflow-auto">
+              <div className="overflow-auto no-scrollbar">
                 <div className="container min-w-fit">
                   <TabsList>
                     {features.map((feature, index) => (
@@ -165,16 +172,17 @@ const Service = () => {
               <div className="pointer-events-none absolute inset-0 bg-[linear-gradient(to_right,hsl(var(--background))_0%,transparent_10%,transparent_90%,hsl(var(--background))_100%)] md:hidden" />
             </div>
             <div className="container mt-12 flex flex-col md:col-span-2 md:mt-20 md:grid md:grid-cols-2 md:gap-6 lg:gap-8">
-              <Carousel setApi={setApi} className="w-full">
+              <Carousel setApi={setApi} className="w-full" plugins={[Fade()]}>
                 <CarouselContent>
                   {features.map((feature) => (
                     <CarouselItem key={feature.id}>
                       <Card>
-                        <CardContent className="flex aspect-[16/9] items-center justify-center p-0">
-                          <img
+                        <CardContent className="flex aspect-[3/2] items-center justify-center p-0">
+                          <Image
                             src={feature.image}
                             alt={feature.title}
                             className="h-full w-full object-cover rounded-lg"
+                            fill
                           />
                         </CardContent>
                       </Card>
@@ -185,11 +193,11 @@ const Service = () => {
 
               {features.map((feature) => (
                 <TabsContent key={feature.id} value={feature.id}>
-                  <div className="flex flex-col justify-center py-8 md:px-8 md:py-10 lg:px-10 lg:py-12">
+                  <div className="flex flex-col justify-center min-h-full py-8 md:py-0 md:px-8 lg:px-10 ">
                     <h3 className="mb-3 text-xl font-semibold md:mb-4 md:text-4xl lg:mb-6">
                       {feature.title}
                     </h3>
-                    <p className="text-muted-foreground lg:text-lg">
+                    <p className="text-muted-foreground text-base lg:text-lg">
                       {feature.description}
                     </p>
                   </div>
