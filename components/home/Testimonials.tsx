f208b3ee9bf47ebea75ba691185b94a5b02a05c9
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-01-30 00:28:29 -0500

    style: Add Quotes from the customer to the data.
    New pictures were also added.

--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -10,74 +10,76 @@ import {
   CarouselContent,
   CarouselItem,
 } from '@/components/ui/carousel';
+import Image from 'next/image';
+import { RiDoubleQuotesL } from 'react-icons/ri';
 
 const data = [
   {
     id: 'item-1',
-    title: '',
-    description: '',
-    href: '#',
+    customer: '',
+    testimonial: '',
     image:
       'https://img.freepik.com/free-photo/modern-clean-interior-design_23-2151929382.jpg?t=st=1737778312~exp=1737781912~hmac=c2f74d669b6971c28bc607f0d4e357e80cd33aa90584a09a7d290c3dc227c21b&w=740',
+    alt: 'Modern clean interior design',
   },
   {
     id: 'item-2',
-    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
-    description:
-      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
-    href: '#',
+    customer: '— Emily Davenport',
+    testimonial:
+      'Drip Painting transformed our home! Their attention to detail was incredible, and the finished result exceeded our expectations. The crew was professional, punctual, and left everything spotless. Highly recommend!',
     image:
       'https://img.freepik.com/free-photo/paint-brush-with-liquid-paint_144627-33542.jpg?t=st=1737776649~exp=1737780249~hmac=fd5702c7de1aaea164eb1fe03ff0a71050963c8e58f3c8aa10261c247e70a5ca&w=740',
+    alt: 'Paint brush with liquid paint',
   },
   {
     id: 'item-3',
-    title: '',
-    description: '',
-    href: '#',
+    customer: '',
+    testimonial: '',
     image:
       'https://img.freepik.com/free-photo/modern-living-room-interior-design_23-2150794726.jpg?t=st=1737778600~exp=1737782200~hmac=5f2f22dc0810de95390c6cb9085ad138374d7a7e82e42c7712f10ac1a8fd9bbe&w=740',
+    alt: 'Modern living room interior design',
   },
   {
     id: 'item-4',
-    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
-    description:
-      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
-    href: '#',
+    customer: '— Mark Reynolds',
+    testimonial:
+      'We hired Drip Painting for both interior and exterior work, and they did a fantastic job. The colors they helped us choose look amazing, and the quality of their work speaks for itself. We’ll definitely be using them again!',
     image:
-      'https://img.freepik.com/premium-photo/red-colour-lipstick-border-with-empty-space-middle_126740-550.jpg?w=740',
+      'https://img.freepik.com/premium-photo/abstract-red-watercolor-white-background_196038-20282.jpg?w=740',
+    alt: 'Red colour lipstick border with empty space in the middle',
   },
   {
     id: 'item-5',
-    title: '',
-    description: '',
-    href: '#',
+    customer: '',
+    testimonial: '',
     image:
       'https://img.freepik.com/free-photo/home-interior-decorated-brown-shades_23-2151934914.jpg?t=st=1737779530~exp=1737783130~hmac=08f038c30595d72e752591c32c77f86de4b4b2c5f61c31505b3e6ede06106bec&w=740',
+    alt: 'Home interior decorated in brown shades',
   },
   {
     id: 'item-6',
-    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
-    description:
-      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
-    href: '#',
+    customer: '— Samantha Quinn',
+    testimonial:
+      'Absolutely thrilled with the custom finish they did in our living room! The team was friendly, efficient, and incredibly skilled. Our walls look like they belong in a design magazine. Thank you, Drip Painting!',
     image:
       'https://img.freepik.com/free-photo/bright-yellow-paint-with-textured_23-2147746859.jpg?t=st=1737781806~exp=1737785406~hmac=f213501e7be1e11355127d789522098c124f817260b39297d484e31e7a177824&w=740',
+    alt: 'Bright yellow paint with textured',
   },
   {
     id: 'item-7',
-    title: '',
-    description: '',
-    href: '#',
+    customer: '',
+    testimonial: '',
     image: 'https://img.freepik.com/premium-photo/kitchen_602868-408.jpg?w=740',
+    alt: 'Kitchen white cabinets and counters',
   },
   {
     id: 'item-8',
-    title: 'Duis sem sem, gravida vel porttitor eu, volutpat ut arcu',
-    description:
-      'Pellentesque eget quam ligula. Sed felis ante, consequat nec ultrices ut, ornare quis metus. Vivamus sit amet tortor vel enim sollicitudin hendrerit.',
-    href: '#',
+    customer: '— Jonathan Pierce',
+    testimonial:
+      'After 20 years in business, it’s no wonder Drip Painting is the best around. They brought our vision to life with expert craftsmanship and a keen eye for detail. A+ service from start to finish!',
     image:
       'https://img.freepik.com/premium-photo/abstract-green-paint-stroke-white-background_1351431-19351.jpg?w=740',
+    alt: 'Abstract green paint stroke on white background',
   },
 ];
 
@@ -125,29 +127,38 @@ const Testimonials = () => {
             }}
           >
             <CarouselContent className=" mr-[1rem] ">
-              {data.map((item) => (
+              {data.map((item, index) => (
                 <CarouselItem
                   key={item.id}
                   className="max-w-[320px] pl-[20px] lg:max-w-[360px]"
                 >
-                  <a href={item.href} className="group rounded-xl">
-                    <div className="group relative h-full min-h-[27rem] max-w-full overflow-hidden rounded-xl bg-red-200 md:aspect-[5/4] lg:aspect-[16/9]">
-                      <img
-                        src={item.image}
-                        alt={item.title}
-                        className="absolute h-full w-full object-cover object-center transition-transform duration-300 group-hover:scale-105"
+                  <div className="group relative h-full min-h-[27rem] max-w-full overflow-hidden rounded-xl bg-red-200 md:aspect-[5/4] lg:aspect-[16/9]">
+                    <Image
+                      src={item.image}
+                      alt={item.alt}
+                      className="absolute h-full w-full object-cover object-center transition-transform duration-300 group-hover:scale-105"
+                      fill
+                    />
+                    {/* <div className="absolute inset-0 h-full bg-[linear-gradient(hsl(var(--primary)/0.2),hsl(var(--primary)/0.8)_100%)] mix-blend-multiply" /> */}
+                    {index % 2 === 1 ? (
+                      <RiDoubleQuotesL
+                        className="absolute left-2 top-[calc(20%)] -skew-x-[20deg]"
+                        size={'3.5rem'}
+                        color="#000"
+                        opacity={0.8}
                       />
-                      {/* <div className="absolute inset-0 h-full bg-[linear-gradient(hsl(var(--primary)/0.2),hsl(var(--primary)/0.8)_100%)] mix-blend-multiply" /> */}
-                      <div className="absolute inset-x-0 top-1/2 -translate-y-1/4 flex flex-col items-start p-6 text-coolGray-dark5 md:p-8">
-                        <div className="mb-2 pt-4 text-xl font-semibold md:mb-3 md:pt-4 lg:pt-4">
-                          {item.title}
-                        </div>
-                        <div className="mb-8 line-clamp-2 md:mb-12 lg:mb-9">
-                          {item.description}
-                        </div>
+                    ) : null}
+                    <div className="absolute inset-x-0 top-1/4 flex-col items-start p-6 text-coolGray-dark5 md:p-8">
+                      <blockquote className="mb-6">
+                        <p className="text-coolGray-dark5 font-semibold text-lg lg:text-xl ">{`${item.testimonial}`}</p>
+                      </blockquote>
+                      <div className=" line-clamp-2 md:mb-12">
+                        <p className="text-base text-coolGray-dark5 text-right">
+                          {item.customer}
+                        </p>
                       </div>
                     </div>
-                  </a>
+                  </div>
                 </CarouselItem>
               ))}
             </CarouselContent>
