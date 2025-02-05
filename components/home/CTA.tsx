4cb8f0378f534e62053db64ddd1b2c6686851769
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-02-04 22:10:22 -0500

    Style: opacity, bg-color, and button updated.

--- components/home/CTA.tsx
+++ components/home/CTA.tsx
@@ -1,5 +1,8 @@
 "use client";
 import dynamic from "next/dynamic";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 const ParallaxContainer = dynamic(
   () => import("@/components/ui/parallax/ParallaxContainer"),
   {
@@ -19,22 +22,25 @@ const CTA = () => {
       <div className="relative">
         <div className="my-14 px-4 py-[clamp(3.75rem,7.82vw,6.25rem)] sm:px-6 sm:py-32 md:my-16 lg:px-8">
           <div className="mx-auto max-w-2xl text-center">
-            <h2 className="sm:text-5x text-balance text-4xl font-semibold tracking-tight text-coolGray-light1">
+            <h2 className="sm:text-5x text-balance text-4xl font-semibold tracking-tight text-coolGray-dark5">
               Bring your vision to life. Get a free quote today
             </h2>
-            <p className="mx-auto mt-6 max-w-xl text-pretty font-headings text-xl/8 font-medium text-coolGray-light2">
+            <p className="mx-auto mt-6 max-w-xl text-pretty font-headings text-xl/8 font-medium text-coolGray-dark4">
               Beautiful interiors and polished exteriors start with the right
               team. Contact us today for expert painting that lasts.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6 font-body">
-              <button
-                className="mx-auto cursor-pointer rounded-md bg-orange-500 px-[1em] py-[.75em] font-body text-lg font-bold text-orange-950 transition hover:bg-orange-700"
-                aria-label="View Gallery of Drip's work"
-              >
-                SCHEDULE TODAY
-              </button>
+              <Button className="font-heading group font-semibold transition-all duration-150 hover:-translate-y-[0.15rem] hover:bg-primary hover:shadow-lg">
+                <Link
+                  href="/contact"
+                  className="flex items-center whitespace-nowrap"
+                >
+                  Start Your Project
+                  <ArrowRight className="ml-1 size-4 transition-transform group-hover:translate-x-1" />
+                </Link>
+              </Button>
             </div>
-            <div className="absolute inset-0 -z-10 bg-[rgba(30,34,40,.5)]" />
+            <div className="absolute -top-1 bottom-0 left-0 right-0 -z-10 bg-[hsla(0,0%,99%,.6)]" />
           </div>
         </div>
       </div>
