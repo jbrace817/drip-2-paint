46c8860c9ebf279e87cffad99f723177e45ea072
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-02-09 23:16:46 -0500

    Add index.html for decap cms per docs
    refactor: add line to page.tsx to redirect a logged in user to the admin page.
    
    refactor: Add additional fields to the config.yml file. It is currently working but I will update it to add page content, blog and widgets.

--- app/(cms)/admin/page.tsx
+++ app/(cms)/admin/page.tsx
@@ -2,36 +2,28 @@
 
 export default function Admin() {
   return (
-    <html
-      lang="en"
+    <div
       dangerouslySetInnerHTML={{
         __html: `
-        <!doctype html>
-  <head>
-   <meta charset="utf-8" />
-   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <link href="/admin/config.yml" type="text/yaml" rel="cms-config-url" />
-   <title>Content Manager</title>
-  </head>
-  <body>
-   <!-- Dashboard script-->
-   <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
-   <!-- netlify identity -->
-   <script is:inline src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-   <!-- Customize the netlify identity widget to refresh on logout preventing users from using the cms when not logged in and then seeing an error -->
-   <script is:inline>netlifyIdentity.on('logout', () => location.reload());</script>
-   <script>
-  if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", (user) => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  }
-</script>
-  </body>`,
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/admin/config.yml" type="text/yaml" rel="cms-config-url" />
+        <title>Content Manager</title>
+        <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
+        <script is:inline src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <script is:inline>netlifyIdentity.on('logout', () => location.reload());</script>
+        <script>
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", (user) => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          });
+        }
+        </script>
+        `,
       }}
     />
   );
