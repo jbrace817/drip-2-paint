b63fcc321fb68b2da967ddcdb1205451c8f9a7aa
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-02-10 13:51:42 -0500

    fix: admin panel went back to initial setup dangerouslysetinnerhtml

--- app/(cms)/admin/page.tsx
+++ app/(cms)/admin/page.tsx
@@ -1,13 +1,30 @@
-"use client"; // Ensure this runs on the client side
+"use client"; // src/app/(cms)/admin/page.tsx
 
 export default function Admin() {
   return (
-    <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
-      <iframe
-        src="/admin/index.html"
-        style={{ width: "100%", height: "100%", border: "none" }}
-        title="Decap CMS"
-      />
-    </div>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/admin/config.yml" type="text/yaml" rel="cms-config-url" />
+        <title>Content Manager</title>
+        <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
+        <script is:inline src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <script is:inline>netlifyIdentity.on('logout', () => location.reload());</script>
+        <script>
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", (user) => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          });
+        }
+        </script>
+        `,
+      }}
+    />
   );
 }
