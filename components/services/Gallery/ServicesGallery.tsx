94ff55df15ba0ce4f18386b4274b7e7e97d0fbe6
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-02-19 21:34:22 -0500

    feature: initial ServicesGallery component

---
+++ components/services/Gallery/ServicesGallery.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+function ServicesGallery() {
+  return (
+    <div className="container mx-auto py-32">
+      <div className="grid grid-rows-none gap-4 md:grid-cols-5"></div>
+    </div>
+  );
+}
+
+export default ServicesGallery;
