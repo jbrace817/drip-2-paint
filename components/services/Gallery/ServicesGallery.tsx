40e92abe6573348f0c045e0b76355ab7a4f9ca1a
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-02-20 22:34:01 -0500

    content: add heading to ServicesGallery component.

--- components/services/Gallery/ServicesGallery.tsx
+++ components/services/Gallery/ServicesGallery.tsx
@@ -12,30 +12,38 @@ function ServicesGallery({ gallery }: GalleryProps) {
   return (
     <section className="my-24 md:my-32">
       <div className="container mx-auto">
+        <div className="text-pretty px-4">
+          <p className="bg-gradient-to-r from-primary-light4 to-primary-dark2 bg-clip-text text-center text-lg/7 font-semibold text-transparent md:text-left">
+            Featured Projects
+          </p>
+          <h3 className="mb-6 text-center text-3xl font-semibold text-coolGray-dark5 md:mb-6 md:text-left md:text-4xl lg:mb-6">
+            Signature Drips
+          </h3>
+        </div>
         <div className="grid auto-rows-[136px] grid-cols-2 gap-4 px-4 md:grid-cols-5 md:px-0 lg:auto-rows-[228px]">
           {gallery.map((image, index) => (
             <div
               key={index}
-              className={`relative ${
+              className={`group relative ${
                 index === 0
                   ? "col-span-2 row-span-2 md:col-span-3 md:row-span-2"
                   : ""
-              } h-full w-full`}
+              } h-full w-full overflow-hidden rounded-lg`}
             >
               <Image
                 src={image.image_upload}
                 alt={`Gallery image ${index + 1}`}
                 fill
-                className="h-full w-full rounded-lg object-cover object-center"
+                className="h-full w-full object-cover object-center transition-transform duration-300 group-hover:scale-105"
               />
               {/* Overlay for last image */}
               {index === gallery.length - 1 && (
-                <div className="absolute inset-0 z-10 rounded-lg bg-black/50">
+                <div className="absolute inset-0 z-10 rounded-lg bg-black/50 group-hover:cursor-pointer">
                   <div className="z-20 flex h-full w-full flex-col items-center justify-center">
                     <p className="text-xl text-coolGray-light1 lg:text-2xl">
                       View More
                     </p>
-                    <CircleArrowRight className="h-8 w-8 text-coolGray-light1 lg:h-12 lg:w-12" />
+                    <CircleArrowRight className="mt-4 h-8 w-8 text-coolGray-light1 transition-all duration-300 group-hover:scale-105 lg:h-12 lg:w-12" />
                   </div>
                 </div>
               )}
