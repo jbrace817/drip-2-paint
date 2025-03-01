f249c0a61087a3f8e75156ef7f0e4891ecbd6f9b
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-02-28 23:37:55 -0500

    replace dots with splatter
    replaced image

--- components/about/WhoWeAre.tsx
+++ components/about/WhoWeAre.tsx
@@ -1,25 +1,21 @@
 import Image from "next/image";
+import Splatter from "../ui/decorative/Splatter";
 
 export default function WhoWeAre() {
   return (
-    <section className="py-16 md:py-24">
+    <section className="pb-14 md:pb-16">
       <div className="container mx-auto">
         <div className="flex flex-wrap items-center">
           {/* Image Grid */}
           <div className="relative order-2 w-full px-4 lg:order-2 lg:w-1/2">
-            {/* Dot Pattern */}
-            <div
-              className="z[-1] absolute left-[35%] top-[50%] hidden h-40 w-60 -translate-x-1/2 -translate-y-1/2 opacity-50 md:block"
-              style={{
-                background: "radial-gradient(#3f78e0 2px, transparent 2.5px)",
-                backgroundSize: "0.75rem 0.75rem",
-              }}
-            />
+            {/* Decorative paint splatter */}
+
+            <Splatter twClass="z[-1] absolute left-[50%] rotate-[90deg] top-[60%] hidden w-[80%] -translate-x-1/2 -translate-y-1/2 md:block " />
 
             {/* Images */}
             <div className="relative mt-[15%] flex flex-col gap-4 md:flex-none">
               <div className="relative md:z-30 md:ml-[30%] md:w-[70%]">
-                <div className="relative aspect-[4/3] overflow-hidden rounded-lg shadow-lg">
+                <div className="relative aspect-[3/2] overflow-hidden rounded-lg shadow-lg md:aspect-[4/3]">
                   <Image
                     src="https://img.freepik.com/premium-photo/contractor-worker-assembling-new-apartment-kitchen-cabinets_1304075-298.jpg?w=1060"
                     alt="About Drip 2 Paint"
@@ -29,9 +25,9 @@ export default function WhoWeAre() {
                 </div>
               </div>
               <div className="relative md:z-40 md:-mt-[15%] md:w-[55%]">
-                <div className="relative aspect-[4/3] overflow-hidden rounded-lg shadow-lg">
+                <div className="relative aspect-[3/2] overflow-hidden rounded-lg shadow-lg md:aspect-[4/3]">
                   <Image
-                    src="https://images.unsplash.com/photo-1546552356-3fae876a61ca?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    src="https://img.freepik.com/premium-photo/close-up-two-beautiful-young-designer-women-working-design-project-while-choosing-materials-office_519356-261.jpg?w=1060"
                     alt="About Drip 2 Paint"
                     fill
                     className="object-cover"
@@ -43,7 +39,7 @@ export default function WhoWeAre() {
 
           {/* Content */}
           <div className="w-full px-4 lg:w-1/2">
-            <h3 className="mt-16 scroll-m-24 text-pretty text-2xl font-semibold tracking-tight sm:text-3xl">
+            <h3 className="scroll-m-24 text-pretty text-2xl font-semibold tracking-tight sm:text-3xl">
               Our Story
             </h3>
             <p className="mt-6 text-lg text-coolGray-dark2">
