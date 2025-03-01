c9a3fbd7d77ba9305aa13aa8a6d7eecbf6980d86
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-02-28 23:39:30 -0500

    updated title of page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ async function AboutPage() {
     <main className="py-0 md:px-4 xl:px-6">
       <div className="rounded-lg bg-gradient-to-b from-coolGray-light2 from-80% to-white px-4 pb-14">
         <h1 className="py-14 text-center text-4xl font-medium md:py-16 md:text-5xl">
-          About Us
+          About Drip Paint
         </h1>
         <div className="container relative mx-auto aspect-video max-w-screen-xl lg:aspect-[5/2]">
           <Image
