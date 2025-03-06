a8161591237fe8db709416a5ad64b3ffe6ab3c3b
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-03-05 22:52:47 -0500

    Add image prop to give the option of changing the bg image

--- components/home/CTA.tsx
+++ components/home/CTA.tsx
@@ -10,13 +10,15 @@ const ParallaxContainer = dynamic(
   },
 );
 
-const CTA = () => {
+const defaultIamge =
+  "https://images.unsplash.com/photo-1669317139155-912572c38362?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+const CTA = ({ image = defaultIamge }) => {
   return (
     <ParallaxContainer
       className="parallax-5"
       style={{
-        backgroundImage:
-          "url(https://images.unsplash.com/photo-1669317139155-912572c38362?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+        backgroundImage: `url(${image})`,
       }}
     >
       <div className="relative">
