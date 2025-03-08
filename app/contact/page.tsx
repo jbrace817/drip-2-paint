2364649643d61e23c89410a3125172d568d68a2b
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-03-08 14:43:04 -0500

    Add initial contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import PageHero from "@/components/PageHero";
+import React from "react";
+
+export default function ContactPage() {
+  return (
+    <main className="py-0 md:px-4 xl:px-6">
+      <PageHero title="Contact Us" />
+    </main>
+  );
+}
