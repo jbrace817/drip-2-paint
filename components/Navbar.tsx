9ba3fb21c7461469aa553a11517362985ca0a885
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-03-16 00:35:53 -0400

    fix (nav): Navbar links working for desktop and mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Book, Menu, Sunset, Trees, X, Zap } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
@@ -9,7 +9,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -27,51 +27,39 @@ import {
 } from "@/components/ui/sheet";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const subMenuItemsOne = [
   {
-    title: "Blog",
-    description: "The latest industry news, updates, and info",
-    icon: <Book className="size-5 shrink-0" />,
-  },
-  {
-    title: "Compnay",
-    description: "Our mission is to innovate and empower the world",
-    icon: <Trees className="size-5 shrink-0" />,
-  },
-  {
-    title: "Careers",
-    description: "Browse job listing and discover our workspace",
-    icon: <Sunset className="size-5 shrink-0" />,
-  },
-  {
-    title: "Support",
+    title: "Interior Painting",
     description:
-      "Get in touch with our support team or visit our community forums",
-    icon: <Zap className="size-5 shrink-0" />,
+      "Transform your space with our expert interior painting services",
+    icon: "https://res.cloudinary.com/dsjx8ner3/image/upload/v1742095025/paint-roller_nd5vno.svg",
+    alt: "Paint Roller",
+    link: "/services/interior-painting-services",
   },
-];
+  {
+    title: "Exterior Painting",
+    description: "Protect your curb appeal with our exterior painting services",
+    icon: "https://res.cloudinary.com/dsjx8ner3/image/upload/v1742095026/house_j3iddq.svg",
+    alt: "Two Story House",
 
-const subMenuItemsTwo = [
-  {
-    title: "Help Center",
-    description: "Get all the answers you need right here",
-    icon: <Zap className="size-5 shrink-0" />,
+    link: "/services/exterior-painting-services",
   },
   {
-    title: "Contact Us",
-    description: "We are here to help you with any questions you have",
-    icon: <Sunset className="size-5 shrink-0" />,
+    title: "Bathroom Remodels",
+    description:
+      "Relax, refresh and renew with our bathroom remodeling services",
+    icon: "https://res.cloudinary.com/dsjx8ner3/image/upload/v1742095024/bathtub_binabp.svg",
+    alt: "Bathtub",
+    link: "/services/custom-bathroom-remodels",
   },
   {
-    title: "Status",
-    description: "Check the current status of our services and APIs",
-    icon: <Trees className="size-5 shrink-0" />,
-  },
-  {
-    title: "Terms of Service",
-    description: "Our terms and conditions for using our services",
-    icon: <Book className="size-5 shrink-0" />,
+    title: "Kitchen Remodels",
+    description: "Create a functional and stylish kitchen with our remodels",
+    icon: "https://res.cloudinary.com/dsjx8ner3/image/upload/v1742095025/kitchen_1606657_bx07gr.svg",
+    alt: "Kitchen",
+    link: "/services/custom-kitchen-remodels",
   },
 ];
 
@@ -120,28 +108,36 @@ const Navbar = () => {
         <div className="container mx-auto px-4">
           <nav className="hidden justify-between lg:flex">
             <div className="flex items-center gap-6">
-              <div className="flex items-center gap-2">
-                <Image
-                  src="https://res.cloudinary.com/dsjx8ner3/image/upload/v1738206290/GradientDrip_wdbdky.svg"
-                  className="w-8"
-                  alt="logo"
-                  width={32}
-                  height={32}
-                />
-              </div>
+              <Link href="/">
+                <div className="flex items-center gap-2">
+                  <Image
+                    src="https://res.cloudinary.com/dsjx8ner3/image/upload/v1738206290/GradientDrip_wdbdky.svg"
+                    className="w-8"
+                    alt="logo"
+                    width={32}
+                    height={32}
+                  />
+                </div>
+              </Link>
             </div>
             <div className="flex items-center gap-6">
-              <a
+              <Link
+                href="/"
                 className="font-headings text-lg font-medium text-coolGray-dark4"
-                href="#"
               >
                 Home
-              </a>
+              </Link>
+              <Link
+                href="/about"
+                className="font-headings text-lg font-medium text-coolGray-dark4"
+              >
+                About Us
+              </Link>
               <NavigationMenu>
                 <NavigationMenuList>
                   <NavigationMenuItem className="text-coolGray-dark4">
                     <NavigationMenuTrigger className="bg-transparent font-headings text-lg font-medium">
-                      Products
+                      Services
                     </NavigationMenuTrigger>
                     <NavigationMenuContent>
                       <ul className="w-80 p-3">
@@ -151,38 +147,16 @@ const Navbar = () => {
                               className={cn(
                                 "flex select-none gap-4 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
                               )}
-                              href="#"
+                              href={item.link}
                             >
-                              {item.icon}
-                              <div>
-                                <div className="text-sm font-semibold">
-                                  {item.title}
-                                </div>
-                                <p className="text-sm leading-snug text-muted-foreground">
-                                  {item.description}
-                                </p>
-                              </div>
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
-                    </NavigationMenuContent>
-                  </NavigationMenuItem>
-                  <NavigationMenuItem className="text-coolGray-dark4">
-                    <NavigationMenuTrigger className="bg-transparent font-headings text-lg font-medium">
-                      Resources
-                    </NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                      <ul className="w-80 p-3">
-                        {subMenuItemsTwo.map((item, idx) => (
-                          <li key={idx}>
-                            <a
-                              className={cn(
-                                "flex select-none gap-4 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                              )}
-                              href="#"
-                            >
-                              {item.icon}
+                              <Image
+                                src={item.icon}
+                                className="size-8 shrink-0"
+                                alt={item.alt}
+                                width={32}
+                                height={32}
+                                priority
+                              />
                               <div>
                                 <div className="text-sm font-semibold">
                                   {item.title}
@@ -200,38 +174,39 @@ const Navbar = () => {
                 </NavigationMenuList>
               </NavigationMenu>
 
-              <a
+              <Link
+                href="/gallery"
                 className="font-headings text-lg font-medium text-coolGray-dark4"
-                href="#"
               >
-                Pricing
-              </a>
-              <a
+                Gallery
+              </Link>
+              <Link
                 className="font-headings text-lg font-medium text-coolGray-dark4"
-                href="#"
+                href="/blog"
               >
                 Blog
-              </a>
+              </Link>
             </div>
 
             <div className="flex gap-2">
-              <Button variant="outline" size="sm">
-                Log in
-              </Button>
-              <Button size="sm">Sign up</Button>
+              <Link href="/contact">
+                <Button size="sm">Contact Us</Button>
+              </Link>
             </div>
           </nav>
           <div className="block lg:hidden">
             <div className="flex items-center justify-between">
-              <div className="flex items-center gap-2">
-                <Image
-                  src="https://res.cloudinary.com/dsjx8ner3/image/upload/v1738206290/GradientDrip_wdbdky.svg"
-                  className="w-6"
-                  alt="logo"
-                  width={32}
-                  height={32}
-                />
-              </div>
+              <Link href="/">
+                <div className="flex items-center gap-2">
+                  <Image
+                    src="https://res.cloudinary.com/dsjx8ner3/image/upload/v1738206290/GradientDrip_wdbdky.svg"
+                    className="w-6"
+                    alt="logo"
+                    width={32}
+                    height={32}
+                  />
+                </div>
+              </Link>
               <Sheet>
                 <SheetTrigger asChild>
                   <Menu size={32} className="text-coolGray-dark5" />
@@ -262,121 +237,71 @@ const Navbar = () => {
                     </SheetTitle>
                   </SheetHeader>
                   <div className="mb-6 mt-24 flex flex-col gap-4">
-                    <a href="#" className="font-semibold">
-                      Home
-                    </a>
+                    <SheetClose asChild>
+                      <Link href="/" className="font-semibold">
+                        Home
+                      </Link>
+                    </SheetClose>
                     <Accordion type="single" collapsible className="w-full">
                       <AccordionItem value="products" className="border-b-0">
-                        <AccordionTrigger className="mb-4 py-0 font-semibold hover:no-underline">
-                          Products
+                        <AccordionTrigger className="py-0 font-semibold hover:no-underline">
+                          Services
                         </AccordionTrigger>
                         <AccordionContent className="mt-2">
                           {subMenuItemsOne.map((item, idx) => (
-                            <a
-                              key={idx}
-                              className={cn(
-                                "flex select-none gap-4 rounded-md p-3 leading-none outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                              )}
-                              href="#"
-                            >
-                              {item.icon}
-                              <div>
-                                <div className="text-sm font-semibold">
-                                  {item.title}
+                            <SheetClose key={idx} asChild>
+                              <Link
+                                className={cn(
+                                  "flex select-none gap-4 rounded-md p-3 leading-none outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                                )}
+                                href={item.link}
+                              >
+                                <Image
+                                  src={item.icon}
+                                  className="size-8 shrink-0"
+                                  alt={item.alt}
+                                  width={32}
+                                  height={32}
+                                  priority
+                                />
+                                <div>
+                                  <div className="text-sm font-semibold">
+                                    {item.title}
+                                  </div>
+                                  <p className="text-sm leading-snug text-muted-foreground">
+                                    {item.description}
+                                  </p>
                                 </div>
-                                <p className="text-sm leading-snug text-muted-foreground">
-                                  {item.description}
-                                </p>
-                              </div>
-                            </a>
-                          ))}
-                        </AccordionContent>
-                      </AccordionItem>
-                      <AccordionItem value="resources" className="border-b-0">
-                        <AccordionTrigger className="py-0 font-semibold hover:no-underline">
-                          Resources
-                        </AccordionTrigger>
-                        <AccordionContent className="mt-2">
-                          {subMenuItemsTwo.map((item, idx) => (
-                            <a
-                              key={idx}
-                              className={cn(
-                                "flex select-none gap-4 rounded-md p-3 leading-none outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                              )}
-                              href="#"
-                            >
-                              {item.icon}
-                              <div>
-                                <div className="text-sm font-semibold">
-                                  {item.title}
-                                </div>
-                                <p className="text-sm leading-snug text-muted-foreground">
-                                  {item.description}
-                                </p>
-                              </div>
-                            </a>
+                              </Link>
+                            </SheetClose>
                           ))}
                         </AccordionContent>
                       </AccordionItem>
                     </Accordion>
-                    <a href="#" className="font-semibold">
-                      Pricing
-                    </a>
-                    <a href="#" className="font-semibold">
-                      Blog
-                    </a>
+
+                    <SheetClose asChild>
+                      <Link href="/about" className="font-semibold">
+                        About Us
+                      </Link>
+                    </SheetClose>
+                    <SheetClose asChild>
+                      <Link href="/gallery" className="font-semibold">
+                        Gallery
+                      </Link>
+                    </SheetClose>
+                    <SheetClose asChild>
+                      <Link href="/blog" className="font-semibold">
+                        Blog
+                      </Link>
+                    </SheetClose>
                   </div>
-                  <div className="border-t py-4">
-                    <div className="grid grid-cols-2 justify-start">
-                      <a
-                        className={cn(
-                          buttonVariants({
-                            variant: "ghost",
-                          }),
-                          "justify-start text-muted-foreground",
-                        )}
-                        href="#"
-                      >
-                        Press
-                      </a>
-                      <a
-                        className={cn(
-                          buttonVariants({
-                            variant: "ghost",
-                          }),
-                          "justify-start text-muted-foreground",
-                        )}
-                        href="#"
-                      >
-                        Contact
-                      </a>
-                      <a
-                        className={cn(
-                          buttonVariants({
-                            variant: "ghost",
-                          }),
-                          "justify-start text-muted-foreground",
-                        )}
-                        href="#"
-                      >
-                        Imprint
-                      </a>
-                      <a
-                        className={cn(
-                          buttonVariants({
-                            variant: "ghost",
-                          }),
-                          "justify-start text-muted-foreground",
-                        )}
-                        href="#"
-                      >
-                        Sitemap
-                      </a>
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-3">
-                    <Button variant="outline">Log in</Button>
-                    <Button>Sign up</Button>
+                  <div className="border-t py-4"></div>
+                  <div className="flex justify-center">
+                    <SheetClose asChild>
+                      <Link className="w-full" href="/contact">
+                        <Button className="w-full">Contact Us</Button>
+                      </Link>
+                    </SheetClose>
                   </div>
                 </SheetContent>
               </Sheet>
@@ -384,14 +309,6 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      {/* <Button
-        variant="outline"
-        size="icon"
-        className="absolute right-4 z-[9999]"
-        onClick={() => setIsOpen(true)}
-      >
-        <Menu className="size-4" />
-      </Button> */}
     </section>
   );
 };
