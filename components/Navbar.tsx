027a8e18a0f5be6cbac25782c448d197c7a842dc
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-03-17 00:31:48 -0400

    Adjust sheet height.
    button and icons are not at the bottom of any size view port

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -219,137 +219,141 @@ const Navbar = () => {
                 <SheetTrigger asChild>
                   <Menu size={32} className="text-coolGray-dark5" />
                 </SheetTrigger>
-                <SheetContent className="overflow-y-auto" side={"right"}>
-                  <SheetHeader>
-                    <SheetTitle>
-                      <div className="flex items-center justify-between">
-                        <div className="flex items-center gap-2">
-                          <Image
-                            src="https://res.cloudinary.com/dsjx8ner3/image/upload/v1738206290/GradientDrip_wdbdky.svg"
-                            className="w-6"
-                            alt="logo"
-                            width={32}
-                            height={32}
-                          />
-                          <div className="flex flex-col font-headings">
-                            <span className="text-2xl font-semibold">DRIP</span>
-                            <span className="-mt-2 text-xs font-semibold">
-                              Paint Co.
-                            </span>
+                <SheetContent
+                  className="flex min-h-screen flex-col justify-between overflow-y-auto"
+                  side={"right"}
+                >
+                  <div>
+                    <SheetHeader>
+                      <SheetTitle>
+                        <div className="flex items-center justify-between">
+                          <div className="flex items-center gap-2">
+                            <Image
+                              src="https://res.cloudinary.com/dsjx8ner3/image/upload/v1738206290/GradientDrip_wdbdky.svg"
+                              className="w-6"
+                              alt="logo"
+                              width={32}
+                              height={32}
+                            />
+                            <div className="flex flex-col font-headings">
+                              <span className="text-2xl font-semibold">
+                                DRIP
+                              </span>
+                              <span className="-mt-2 text-xs font-semibold">
+                                Paint Co.
+                              </span>
+                            </div>
                           </div>
+                          <SheetClose>
+                            <X size={28} className="text-coolGray-dark5" />
+                          </SheetClose>
                         </div>
-                        <SheetClose>
-                          <X size={28} className="text-coolGray-dark5" />
-                        </SheetClose>
-                      </div>
-                    </SheetTitle>
-                  </SheetHeader>
-                  <div className="mb-6 mt-24 flex flex-col gap-4">
-                    <SheetClose asChild>
-                      <Link href="/" className="font-semibold">
-                        Home
-                      </Link>
-                    </SheetClose>
-                    <Accordion type="single" collapsible className="w-full">
-                      <AccordionItem value="products" className="border-b-0">
-                        <AccordionTrigger className="py-0 font-semibold hover:no-underline">
-                          Services
-                        </AccordionTrigger>
-                        <AccordionContent className="mt-2">
-                          {subMenuItemsOne.map((item, idx) => (
-                            <SheetClose key={idx} asChild>
-                              <Link
-                                className={cn(
-                                  "flex select-none gap-4 rounded-md p-3 leading-none outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                                )}
-                                href={item.link}
-                              >
-                                <Image
-                                  src={item.icon}
-                                  className="size-8 shrink-0"
-                                  alt={item.alt}
-                                  width={32}
-                                  height={32}
-                                  priority
-                                />
-                                <div>
-                                  <div className="text-sm font-semibold">
-                                    {item.title}
+                      </SheetTitle>
+                    </SheetHeader>
+                    <div className="mb-6 mt-24 flex flex-col gap-4">
+                      <SheetClose asChild>
+                        <Link href="/" className="font-semibold">
+                          Home
+                        </Link>
+                      </SheetClose>
+                      <Accordion type="single" collapsible className="w-full">
+                        <AccordionItem value="products" className="border-b-0">
+                          <AccordionTrigger className="py-0 font-semibold hover:no-underline">
+                            Services
+                          </AccordionTrigger>
+                          <AccordionContent className="mt-2">
+                            {subMenuItemsOne.map((item, idx) => (
+                              <SheetClose key={idx} asChild>
+                                <Link
+                                  className={cn(
+                                    "flex select-none gap-4 rounded-md p-3 leading-none outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                                  )}
+                                  href={item.link}
+                                >
+                                  <Image
+                                    src={item.icon}
+                                    className="size-8 shrink-0"
+                                    alt={item.alt}
+                                    width={32}
+                                    height={32}
+                                    priority
+                                  />
+                                  <div>
+                                    <div className="text-sm font-semibold">
+                                      {item.title}
+                                    </div>
+                                    <p className="text-sm leading-snug text-muted-foreground">
+                                      {item.description}
+                                    </p>
                                   </div>
-                                  <p className="text-sm leading-snug text-muted-foreground">
-                                    {item.description}
-                                  </p>
-                                </div>
-                              </Link>
-                            </SheetClose>
-                          ))}
-                        </AccordionContent>
-                      </AccordionItem>
-                    </Accordion>
+                                </Link>
+                              </SheetClose>
+                            ))}
+                          </AccordionContent>
+                        </AccordionItem>
+                      </Accordion>
 
-                    <SheetClose asChild>
-                      <Link href="/about" className="font-semibold">
-                        About Us
-                      </Link>
-                    </SheetClose>
-                    <SheetClose asChild>
-                      <Link href="/gallery" className="font-semibold">
-                        Gallery
-                      </Link>
-                    </SheetClose>
-                    <SheetClose asChild>
-                      <Link href="/blog" className="font-semibold">
-                        Blog
-                      </Link>
-                    </SheetClose>
-                  </div>
-                  <div className="border-t py-4"></div>
-                  <div className="flex h-1/2 flex-col justify-between">
-                    <div className="mb-6">
+                      <SheetClose asChild>
+                        <Link href="/about" className="font-semibold">
+                          About Us
+                        </Link>
+                      </SheetClose>
+                      <SheetClose asChild>
+                        <Link href="/gallery" className="font-semibold">
+                          Gallery
+                        </Link>
+                      </SheetClose>
+                      <SheetClose asChild>
+                        <Link href="/blog" className="font-semibold">
+                          Blog
+                        </Link>
+                      </SheetClose>
+                    </div>
+                    <div className="border-t py-4">
                       <SheetClose asChild>
                         <ContactInfo variant="navbar" />
                       </SheetClose>
                     </div>
-                    <div>
-                      <div className="mb-6 flex justify-center">
-                        <SheetClose asChild>
-                          <Link className="w-full" href="/contact">
-                            <Button className="w-full">Contact Us</Button>
-                          </Link>
-                        </SheetClose>
-                      </div>
-                      <div className="flex justify-center">
-                        <ul className="flex items-center space-x-6 text-coolGray-dark3">
-                          <li className="font-medium hover:text-primary">
-                            <SheetClose asChild>
-                              <Link href="#">
-                                <FaInstagram className="size-6" />
-                              </Link>
-                            </SheetClose>
-                          </li>
-                          <li className="font-medium hover:text-primary">
-                            <SheetClose asChild>
-                              <Link href="#">
-                                <FaFacebook className="size-6" />
-                              </Link>
-                            </SheetClose>
-                          </li>
-                          <li className="font-medium hover:text-primary">
-                            <SheetClose asChild>
-                              <Link href="#">
-                                <FaTwitter className="size-6" />
-                              </Link>
-                            </SheetClose>
-                          </li>
-                          <li className="font-medium hover:text-primary">
-                            <SheetClose asChild>
-                              <Link href="#">
-                                <FaLinkedin className="size-6" />
-                              </Link>
-                            </SheetClose>
-                          </li>
-                        </ul>
-                      </div>
+                  </div>
+
+                  {/* Footer content that sticks to bottom */}
+                  <div className="mt-auto space-y-6 pb-8">
+                    <SheetClose asChild>
+                      <Link className="block w-full" href="/contact">
+                        <Button className="w-full">Contact Us</Button>
+                      </Link>
+                    </SheetClose>
+                    <div className="flex justify-center">
+                      <ul className="flex items-center space-x-6 text-coolGray-dark3">
+                        <li className="font-medium hover:text-primary">
+                          <SheetClose asChild>
+                            <Link href="#">
+                              <FaInstagram className="size-6" />
+                            </Link>
+                          </SheetClose>
+                        </li>
+                        <li className="font-medium hover:text-primary">
+                          <SheetClose asChild>
+                            <Link href="#">
+                              <FaFacebook className="size-6" />
+                            </Link>
+                          </SheetClose>
+                        </li>
+                        <li className="font-medium hover:text-primary">
+                          <SheetClose asChild>
+                            <Link href="#">
+                              <FaTwitter className="size-6" />
+                            </Link>
+                          </SheetClose>
+                        </li>
+                        <li className="font-medium hover:text-primary">
+                          <SheetClose asChild>
+                            <Link href="#">
+                              <FaLinkedin className="size-6" />
+                            </Link>
+                          </SheetClose>
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </SheetContent>
