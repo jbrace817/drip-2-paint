f5be5e433456ac4f1318f13f65519ec1dbc39999
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-03-17 21:58:59 -0400

    fix: statically serving the logos, better performance
    improved load times and caching.

--- components/home/Logos.tsx
+++ components/home/Logos.tsx
@@ -2,66 +2,69 @@
 
 import AutoScroll from "embla-carousel-auto-scroll";
 
+//Logos
+import valspar from "@/public/images/marquee/valspar.svg";
+import benjaminMoore from "@/public/images/marquee/Benjamin-Moore.svg";
+import dutchBoy from "@/public/images/marquee/DutchBoy.svg";
+import behr from "@/public/images/marquee/behr.svg";
+import sherwinWilliams from "@/public/images/marquee/sherwin.svg";
+import ppgIndustries from "@/public/images/marquee/ppg-industries.svg";
+import kilzLogo from "@/public/images/marquee/kilz.svg";
+
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
+import Image from "next/image";
 
 const logos = [
   {
     id: "logo-1",
-    description: "Logo 1",
-    image:
-      "https://res.cloudinary.com/dsjx8ner3/image/upload/v1738384495/valspar_ib5lyh.svg",
+    description: "Valspar",
+    image: valspar,
   },
   {
     id: "logo-2",
-    description: "Logo 2",
-    image:
-      "https://res.cloudinary.com/dsjx8ner3/image/upload/v1738384410/benjamin-moore-paints-1_lybhn3.svg",
+    description: "Benjamin Moore",
+    image: benjaminMoore,
   },
   {
     id: "logo-3",
-    description: "Logo 3",
-    image:
-      "https://res.cloudinary.com/dsjx8ner3/image/upload/v1738384411/DutchBoy_Logo_Fillable_wu2t2b.svg",
+    description: "Dutch Boy",
+    image: dutchBoy,
   },
   {
     id: "logo-4",
-    description: "Logo 4",
-    image:
-      "https://res.cloudinary.com/dsjx8ner3/image/upload/v1738384410/behr_rpzmi2.svg",
+    description: "Behr",
+    image: behr,
   },
   {
     id: "logo-5",
-    description: "Logo 5",
-    image:
-      "https://res.cloudinary.com/dsjx8ner3/image/upload/v1738384409/sherwin-williams-2_tyk3ha.svg",
+    description: "Sherwin Williams",
+    image: sherwinWilliams,
   },
   {
     id: "logo-6",
-    description: "Logo 6",
-    image:
-      "https://res.cloudinary.com/dsjx8ner3/image/upload/v1738384408/ppg-industries_pgfo0r.svg",
+    description: "PPG Industries",
+    image: ppgIndustries,
   },
   {
     id: "logo-7",
-    description: "Logo 7",
-    image:
-      "https://res.cloudinary.com/dsjx8ner3/image/upload/v1738384407/kilz-logo_abq76d.svg",
+    description: "Kilz",
+    image: kilzLogo,
   },
 ];
 
 const Logos = () => {
   return (
-    <section className="bg-white pb-36 pt-5">
+    <section className="bg-white pb-14 pt-5 md:pb-16">
       <div className="container mx-auto flex flex-col items-center text-center">
         <h1 className="my-4 text-pretty text-2xl font-bold lg:text-4xl">
           Our Partnered Brands
         </h1>
       </div>
-      <div className="pt-10 md:pt-16 lg:pt-20">
+      <div className="pt-6 lg:pt-14">
         <div className="container pointer-events-none relative mx-auto flex items-center justify-center overflow-hidden">
           <Carousel
             opts={{ loop: true }}
@@ -74,17 +77,20 @@ const Logos = () => {
             ]}
           >
             <CarouselContent className="ml-0">
-              {logos.map((logo) => (
+              {logos.map((logo, index) => (
                 <CarouselItem
                   key={logo.id}
                   className="basis-1/3 pl-0 sm:basis-1/4 md:basis-1/5 lg:basis-1/6"
                 >
                   <div className="mx-10 flex shrink-0 items-center justify-center">
                     <div>
-                      <img
+                      <Image
                         src={logo.image}
                         alt={logo.description}
                         className="h-28 w-36"
+                        width={144}
+                        height={144}
+                        priority={index < 3}
                       />
                     </div>
                   </div>
--- next.config.mjs
+++ next.config.mjs
@@ -4,30 +4,43 @@ const nextConfig = {
     minimumCacheTTL: 31536000,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/dsjx8ner3/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dsjx8ner3/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-        pathname: '**',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'img.freepik.com',
-        port: '',
-        pathname: '**',
+        protocol: "https",
+        hostname: "img.freepik.com",
+        port: "",
+        pathname: "**",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/images/:path*.svg",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 };
 
