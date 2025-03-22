56109b5b7a05407b5039155dc792dcf79c64f213
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-03-21 22:45:57 -0400

    Add Paralax on scroll with framer-motion

--- components/ui/decorative/Splatter.tsx
+++ components/ui/decorative/Splatter.tsx
@@ -1,13 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import splatter from "@/public/images/decorative/splatter.svg";
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
 
 type SplatterProps = {
   twClass: string;
 };
 
 export default function Splatter({ twClass }: SplatterProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start 80%", "end 20%"],
+  });
+  const y = useTransform(scrollYProgress, [0, 1], [0, 100]);
+
   return (
-    <div aria-hidden="true" className="absolute inset-0">
+    <motion.div
+      ref={containerRef}
+      style={{ y }}
+      aria-hidden="true"
+      className="absolute inset-0 z-[-1]"
+    >
       <Image
         src={splatter}
         alt=""
@@ -15,6 +32,6 @@ export default function Splatter({ twClass }: SplatterProps) {
         height={384} //og - 384
         className={twClass}
       />
-    </div>
+    </motion.div>
   );
 }
