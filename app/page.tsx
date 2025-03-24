fbc602059090b1a22729d0bbf74e7c726945101e
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-03-23 21:40:20 -0400

    test next/dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import CTA from "@/components/home/CTA";
 import Hero from "@/components/home/Hero";
 import Logos from "@/components/home/Logos";
-import Service from "@/components/home/Service";
-import Testimonials from "@/components/home/Testimonials";
 
-import Why from "@/components/home/Why";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
+
+import dynamic from "next/dynamic";
+const Why = dynamic(() => import("@/components/home/Why"), { ssr: false });
+const CTA = dynamic(() => import("@/components/home/CTA"), { ssr: false });
+const Service = dynamic(() => import("@/components/home/Service"), {
+  ssr: false,
+});
+const Testimonials = dynamic(() => import("@/components/home/Testimonials"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
