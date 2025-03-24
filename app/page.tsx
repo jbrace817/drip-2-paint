d85d43a63b6a5374d49fb9d4a39a9a5b0bcb9ded
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2025-03-23 23:08:47 -0400

    refactor: dynamic load for client components placed in Array map method

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ import Logos from "@/components/home/Logos";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 
 import dynamic from "next/dynamic";
-const Why = dynamic(() => import("@/components/home/Why"), { ssr: false });
-const CTA = dynamic(() => import("@/components/home/CTA"), { ssr: false });
-const Service = dynamic(() => import("@/components/home/Service"), {
-  ssr: false,
-});
-const Testimonials = dynamic(() => import("@/components/home/Testimonials"), {
-  ssr: false,
-});
+const Why = dynamic(() => import("@/components/home/Why"));
+
+const [CTA, Service, Testimonials] = ["CTA", "Service", "Testimonials"].map(
+  (component) =>
+    dynamic(() => import(`@/components/home/${component}`), { ssr: false }),
+);
 
 export default function Home() {
   return (
